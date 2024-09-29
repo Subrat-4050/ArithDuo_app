53acf69c57dde61f2a934bb8030820256ddbc323
Subrat-4050 <sahusubrat83184@gmail.com>
2024-09-29 10:57:05 +0530

    Add Functionalities

--- app/src/main/java/com/example/arithduo/MainActivity.java
+++ app/src/main/java/com/example/arithduo/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.arithduo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,59 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText fNum, sNum, operator;
+    TextView res;
+    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        fNum = findViewById(R.id.Num1);
+        sNum = findViewById(R.id.Num2);
+        operator = findViewById(R.id.Operator);
+        res = findViewById(R.id.Result);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1 = fNum.getText().toString();
+                String s2 = sNum.getText().toString();
+
+                int num1 = Integer.parseInt(s1);
+                int num2 = Integer.parseInt(s2);
+
+                switch (operator.getText().toString()) {
+                    case "+":
+                        res.setText(String.valueOf(num1 + num2));
+                        break;
+                    case "-":
+                        res.setText(String.valueOf(num1 - num2));
+                        break;
+                    case "*":
+                        res.setText(String.valueOf(num1 * num2));
+                        break;
+                    case "/":
+                        if (num2 != 0) {
+                            res.setText(String.valueOf(num1 / num2));
+                        } else {
+                            res.setText("Division by zero is not allowed");
+                        }
+                        break;
+                    case "%":
+                        if (num2 != 0) {
+                            res.setText(String.valueOf(num1 % num2));
+                        } else {
+                            res.setText("Division by zero is not allowed");
+                        }
+                        break;
+                    default:
+                        res.setText("Invalid Operator");
+                        break;
+                }
+            }
         });
     }
 }
